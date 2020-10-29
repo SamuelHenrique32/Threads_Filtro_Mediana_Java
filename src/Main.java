c6da3f6e3b5335511020d3d56fcc192d9d749bc3
Samuel Dalmas <samuel.dalmas@mobitec.com.br>
2020-10-28 23:00:33 -0300

    Adding Basic Files

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+package test;
+
+
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		File file = new File("./out.bmp");
+		
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("borboleta.bmp"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// you should stop here
+		byte[][] green = new byte[30][40];
+		for(int x=0; x<30; x++){
+		  for(int y=0; y<40; y++){
+		     int color = img.getRGB(x,y);
+		     //alpha[x][y] = (byte)(color>>24);
+		     //red[x][y] = (byte)(color>>16);
+		     green[x][y] = (byte)(color>>8);
+		     //blue[x][y] = (byte)(color);
+		  }
+		}
+		
+		try {
+			ImageIO.write(img, "BMP", file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+}
